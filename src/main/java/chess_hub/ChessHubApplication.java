bce2d858b31dc38ad1150d1258cf3c6656aaa53c
Netsai <hellonetsai@gmail.com>
2026-06-24 21:29:43 +0200

    Member entity & folder structure

---
+++ src/main/java/chess_hub/ChessHubApplication.java
@@ -0,0 +1,13 @@
+package chess_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessHubApplication.class, args);
+	}
+
+}
---
+++ src/main/java/chess_hub/entity/Member.java
@@ -0,0 +1,58 @@
+package chess_hub.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private Integer rating;
+
+    public Member() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+}
